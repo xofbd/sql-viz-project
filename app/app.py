6ef8b822de53448bfa82e390166e085d5f10c917
xofbd <dfox09@gmail.com>
2021-07-25 22:22:41 -0400

    Add app.py module for running Flask application

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+from database import query_db
+from plot import plot_wells
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+@app.route('/plot', methods=['POST'])
+def plot():
+    depth_min = request.form['depth_min']
+    grad_min = request.form['grad_min']
+
+    data = query_db(depth_min, grad_min)
+    chart_json = plot_wells(data)
+
+    return render_template('plot.html', chart=chart_json)
